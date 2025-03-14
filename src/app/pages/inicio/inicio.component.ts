d6432b858930e844aed6bab057d58920cd776529
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-03-14 13:57:49 -0600

    refactor: remove header and footer components, add login component to inicio page

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { LoginComponent } from '../../components/login/login.component';
+
 
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink],
+  imports: [HeaderComponent, LoginComponent, FooterComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
