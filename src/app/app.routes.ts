fd9dd57d1edf3ce0879001dc7d647e1d22dd1949
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-03-17 15:56:30 -0600

    feat: update routing to set DashboardComponent as default view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-{path:'', component: InicioComponent},
-{path:'dashboard', component: DashboardComponent}
+{path:'', component: DashboardComponent},
+//{path:'dashboard', component: DashboardComponent}
 
 
 ];
