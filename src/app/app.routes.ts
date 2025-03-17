e2c65b2d33aa34f394f8695404dffed3b727be3c
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2025-03-17 16:20:15 -0600

    feat: update dashboard layout and routing for future login integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  //TODO: Cambiar a LoginComponent
 {path:'', component: DashboardComponent},
 //{path:'dashboard', component: DashboardComponent}
-
-
 ];
